2aef545ef68b8d87c12dc7e1e6015cc1c14fdf4f
Ashish Awaghad <ashish@Ashishs-MacBook-Pro.local>
2013-04-15 12:02:44 +0800

    Initial Commit

---
+++ FacebookCircles/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  FacebookCircles
+//
+//  Created by Ashish Awaghad on 15/4/13.
+//  Copyright (c) 2013 Ashish Awaghad. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ FacebookCircles/MasterViewController.h
@@ -0,0 +1,13 @@
+//
+//  MasterViewController.h
+//  FacebookCircles
+//
+//  Created by Ashish Awaghad on 15/4/13.
+//  Copyright (c) 2013 Ashish Awaghad. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MasterViewController : UITableViewController
+
+@end
